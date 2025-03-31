6a0aea14c65b4a8518ae6e8cb83f956b1d16dd96
황찬호 <cksgh0316@gmail.com>
2025-03-31 14:27:13 +0900

    Initial Commit

---
+++ Source/PlatformFighterStarterKit/TESTMyActor.cpp
@@ -0,0 +1,27 @@
+
+
+
+#include "TESTMyActor.h"
+
+// Sets default values
+ATESTMyActor::ATESTMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATESTMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATESTMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
