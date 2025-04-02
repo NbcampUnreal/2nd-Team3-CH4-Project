508541787215b654c48ea8b867cd24f77162cf78
seunghyeonL <hkr0416@gmail.com>
2025-04-02 20:45:36 +0900

    [Add] Character, Charcter Components, PlayerController Base Structure

---
+++ Source/WeaponMaster/Characters/Components/ISkillSystemUser.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "ISkillSystemUser.generated.h"
+
+/**
+ * 
+ */
+UINTERFACE(BlueprintType)
+class WEAPONMASTER_API USkillSystemUser : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class WEAPONMASTER_API ISkillSystemUser
+{
+	GENERATED_BODY()
+
+public:
+	// SkillComponent에서 Character에 접근할 메서드
+	// ItemComponent에서 Character에 접근할 메서드
+	// SkillComponent에서 ITemComponent에 접근할 메서드
+	// ItemComponent에서 SkillComponent에 접근할 메서드
+};
---
+++ Source/WeaponMaster/Characters/States/IStateUser.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "IState.h"
+#include "UObject/Interface.h"
+#include "IStateUser.generated.h"
+
+/**
+ * 
+ */
+UINTERFACE(BlueprintType)
+class WEAPONMASTER_API UStateUser : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class WEAPONMASTER_API IStateUser
+{
+	GENERATED_BODY()
+	virtual void SetState(TObjectPtr<IState> NextState) = 0;
+};
