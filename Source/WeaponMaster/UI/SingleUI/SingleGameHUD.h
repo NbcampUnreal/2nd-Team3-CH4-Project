54bc979bd31bf1a023cc2a5cf1d60e06a1419bc7
KKuBong <165163630+KKuBong@users.noreply.github.com>
2025-04-06 17:02:34 +0900

    [Add] SingleGameHUD

---
+++ Source/WeaponMaster/UI/SingleUI/SingleGameHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "SingleGameHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WEAPONMASTER_API ASingleGameHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
