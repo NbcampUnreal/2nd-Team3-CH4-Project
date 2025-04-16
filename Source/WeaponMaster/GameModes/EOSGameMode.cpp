b5e90c752c308be8296c849b089df3075baf0d78
황찬호 <cksgh0316@gmail.com>
2025-04-17 00:49:54 +0900

    [Merge] To merge

--- Source/WeaponMaster/GameModes/EOSGameMode.cpp
+++ Source/WeaponMaster/GameModes/EOSGameMode.cpp
@@ -189,7 +189,7 @@ void AEOSGameMode::TravelMap()
 {
 	UE_LOG(LogTemp, Warning, TEXT("TravelMap To [%s]"), *MapURL);
 	if (MapURL == "") return;
-	GetWorld()->ServerTravel(MapURL, true);
+	GetWorld()->SeamlessTravel(MapURL, true);
 }
 
 void AEOSGameMode::StartSession()
